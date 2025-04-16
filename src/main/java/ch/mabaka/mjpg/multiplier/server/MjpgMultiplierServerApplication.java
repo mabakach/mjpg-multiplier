3cb430ca1dbe18e721529c56f19c382b6b137de4
Marc Baumgartner <marc@mabaka.ch>
2025-04-16 06:14:53 +0200

    Project created

---
+++ src/main/java/ch/mabaka/mjpg/multiplier/server/MjpgMultiplierServerApplication.java
@@ -0,0 +1,13 @@
+package ch.mabaka.mjpg.multiplier.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjpgMultiplierServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjpgMultiplierServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/mabaka/mjpg/multiplier/server/MjpgMultiplierServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.mabaka.mjpg.multiplier.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjpgMultiplierServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
