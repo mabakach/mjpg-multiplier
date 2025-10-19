d7ffe8ee3c4f5abbb0b2e5878457c1cac6b21a7c
Marc Baumgartner <marc@mabaka.ch>
2025-10-19 12:02:13 +0200

    Fix pom and relase.sh

--- release.sh
+++ release.sh
@@ -10,6 +10,13 @@ mvn release:perform
 # Step 3: Check out the release tag
 RELEASE_TAG=$(git describe --tags --abbrev=0)
 git checkout $RELEASE_TAG
+# Restore pom.xml from the tag to match the released version
+git checkout $RELEASE_TAG -- pom.xml
+# Clean up Maven release plugin backup files
+rm -f pom.xml.releaseBackup release.properties
 
 # Step 4: Build the Docker image tar with the release version
 mvn clean verify jib:buildTar
+
+# Step 5: Return to master branch
+git checkout master
