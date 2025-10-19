8f0e4e12e2d7c2d9bc9d626f64b75158e02cc9b6
Marc Baumgartner <marc@mabaka.ch>
2025-10-19 11:53:25 +0200

    Fix release.sh according to Copilot hint

--- release.sh
+++ release.sh
@@ -1,17 +1,15 @@
-
 #!/bin/bash
 rm -rf ./target
 
 # Step 1: Prepare the release (updates the version and creates a tag)
 mvn release:prepare
 
-# Step 2: Check out the release tag
+# Step 2: Perform the release
+mvn release:perform
+
+# Step 3: Check out the release tag
 RELEASE_TAG=$(git describe --tags --abbrev=0)
 git checkout $RELEASE_TAG
 
-# Step 3: Build the Docker image tar with the release version
+# Step 4: Build the Docker image tar with the release version
 mvn clean verify jib:buildTar
-
-# Step 4: Switch back to the master branch and perform the release
-git checkout master
-mvn release:perform
