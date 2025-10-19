5b6bcdac0cb0a3c8a02dc1bf0b0079f6b30b818d
Marc Baumgartner <marc@mabaka.ch>
2025-10-19 12:52:57 +0200

    Get branch from current branch and commit pom.xml at the end of the release

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 rm -rf ./target
 
+# Get the current branch name
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 # Step 1: Prepare the release (updates the version and creates a tag)
 mvn release:prepare
 
@@ -18,5 +21,12 @@ rm -f pom.xml.releaseBackup release.properties
 # Step 4: Build the Docker image tar with the release version
 mvn clean verify jib:buildTar
 
-# Step 5: Return to master branch
-git checkout master
+# Step 5: Return to the original branch
+git checkout $CURRENT_BRANCH
+
+# Step 6: Commit and push pom.xml if modified
+if [[ $(git status --porcelain pom.xml) ]]; then
+  git add pom.xml
+  git commit -m "Update pom.xml to next snapshot version after release"
+  git push
+fi
