04893f55bdb6e02c145e82d298e10fdc6388d6a5
Marc Baumgartner <marc@mabaka.ch>
2025-10-21 20:56:36 +0200

    Simplify release.sh for tests

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 rm -rf ./target
 
 # Get the current branch name
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Step 1: Prepare the release (updates the version and creates a tag)
 mvn release:prepare
@@ -11,20 +11,20 @@ mvn release:prepare
 mvn release:perform
 
 # Step 3: Check out the release tag
-RELEASE_TAG=$(git describe --tags --abbrev=0)
-git checkout $RELEASE_TAG
+#RELEASE_TAG=$(git describe --tags --abbrev=0)
+#git checkout $RELEASE_TAG
 # Clean up Maven release plugin backup files
-rm -f pom.xml.releaseBackup release.properties
+#rm -f pom.xml.releaseBackup release.properties
 
 # Step 4: Build the Docker image tar with the release version
-mvn clean verify jib:buildTar
+#mvn clean verify jib:buildTar
 
 # Step 5: Return to the original branch
-git checkout $CURRENT_BRANCH
+#git checkout $CURRENT_BRANCH
 
 # Step 6: Commit and push pom.xml if modified
-if [[ $(git status --porcelain pom.xml) ]]; then
-  git add pom.xml
-  git commit -m "Update pom.xml to next snapshot version after release"
-  git push
-fi
+#if [[ $(git status --porcelain pom.xml) ]]; then
+#  git add pom.xml
+#  git commit -m "Update pom.xml to next snapshot version after release"
+#  git push
+#fi
